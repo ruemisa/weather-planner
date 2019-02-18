0f473fc146ee9f49a9495223147d6b0ce5e86bcc
Ruel Misa <ruel.g.misa@gmail.com>
2019-02-18 12:58:15 -0500

    integrated device geolocation api to app in order to grab current user's device location

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    state = {
+        location: {
+            lat: null,
+            lon: null
+        }
+    }
+
+    componentDidMount() {
+        // NOTE: Consider using watchPosition to track user's position live and clearWatch in componentDidUnmount for cleanup
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                const { latitude, longitude } = position.coords;
+                this.setState({
+                    location: {
+                        lat: latitude,
+                        lon: longitude
+                    }
+                });
+            },
+            error => console.log(error)
+        )
+    }
+    
+
     render() {
+        console.log(this.state.location.lat)
+
         return (
             <div>
                 Weather Planner
