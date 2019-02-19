692e74099340ac6afe13c85559bf743c0f1bd9cf
Ruel Misa <ruel.g.misa@gmail.com>
2019-02-19 12:24:30 -0500

    created basic reducer. Hooked up reducer to store in app's root index.js. Wrapped App component inside Provider from react-redux to make use of redux store

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+    // NOTE: Thinking of spreading this object out into just lat and lon
+    location: {
+        lat: null,
+        lon: null
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+};
+
+export default reducer;
