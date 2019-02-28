3d4db4e57d228d9ff4d20dd6d7f2100ffdb34d8b
Ruel Misa <ruel.g.misa@gmail.com>
2019-02-28 13:30:13 -0500

    added reducer utility file that contains reusable code for all redux state update

--- src/reducers/weatherReducer.js
+++ src/reducers/weatherReducer.js
@@ -1,21 +1,22 @@
 import { GET_WEATHER } from '../actions/types';
+import { updateObject } from '../utils/reducerUtils';
 
 const initialState = {};
+
 
 
 export default (state = initialState, action) => {
     // GET WEATHER based on User Location
     switch (action.type) {
         case GET_WEATHER:
-            return {
-                ...state,
+            return updateObject(state, {
                 weather: {
                     ...state.weather,
                     // TODO: Refactor this outside of this function
                     currentTemp: action.payload.currently.temperature,
                     summary: action.payload.currently.summary
                 }
-            }
+            });
         
         default:
             return state;
